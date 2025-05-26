74f0454fe09ee7c4ea697fd01094b5cabbe4de10
salsarizkii <salsarizkisaputri@student.uns.ac.id>
2025-05-26 23:20:39 +0700

    Refactor NewsController and showNews view: streamline image handling, enhance news display, and add action buttons for editing and deleting news items

--- resources/views/admin/news/showNews.blade.php
+++ resources/views/admin/news/showNews.blade.php
@@ -15,6 +15,7 @@
                   <tr>
                       <th scope="col" width="5%">No</th>
                       <th scope="col" width="75%" class="text-left">Aduan</th>
+                      <th scope="col" width="20%" class="text-left">Aksi</th>
                   </tr>
               </thead>
               <tbody>
@@ -25,10 +26,10 @@
                               <p class="my-0"><strong>{{ $new->title }}</strong></p><p class="my-0 text-muted" style="font-size: 0.75rem;">
                                 {{ $new->created_at->translatedFormat('d M Y') }}
                             </p>
-                            <img src="{{ asset('/images/news/' . $new->image_path) }}" alt="Gambar">
+                            <img src="{{ asset('/images/news/' . $new->image_path) }}" alt="Gambar" weight="20" height="20" class="img-fluid mb-2">
 
                             
-                              <p class="my-0">{{ $new->description }}</p>
+                              <p class="my-0">{{ Str::limit($new->description, 200, '...') }}</p>
                               <p class="my-0 text-muted" style="font-size: 0.75rem;">
                                 {{ $new->created_at->format('H.i') }}
                             </p>
@@ -40,6 +41,17 @@
                             </div>
 
                           </td>
+                              <td class="align-middle" style="white-space: nowrap;">
+                                <i class="bi bi-pencil-square"></i>
+                                <a href=#
+                                   class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover text-secondary"
+                                   style="font-size: 0.75rem;"> edit</a>
+                                <i class="bi bi-trash3-fill"></i>
+                                <form method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-link text-secondary p-0" style="font-size: 0.75rem;">hapus</button>
+                              </td>
 
                       </tr>
                   @endforeach
